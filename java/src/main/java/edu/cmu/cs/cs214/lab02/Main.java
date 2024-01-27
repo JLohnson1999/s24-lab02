136cee28ff6a6168d8ff76517036c2d3b0bc8273
琦翔羅 <johnson@qixiangluos-MacBook-Pro.local>
2024-01-26 23:34:18 -0500

    the previous one is the wrong one, this is correct

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -5,6 +5,8 @@ import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
+        // change to interface
+        // interface can be use as parent class(canonly use parent's functions)
         Shape shapes = new Rectangle(2, 3);
 
         Renderer renderer = new Renderer(shapes);
